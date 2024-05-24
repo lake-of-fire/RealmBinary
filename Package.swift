203b9e62ac56c31d24365132255d5d10dc1fab81
Alex Ehlke <alex.ehlke@gmail.com>
2024-05-24 19:18:47 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -17,17 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.48.0/Realm.spm.zip",
-            checksum: "bb63b20a9d0b6651777c16549006a11af6b7556db6e2991ed57eadda62974979"
-//            url: "https://github.com/realm/realm-swift/releases/download/v10.46.0/Realm.spm.zip",
-//            checksum: "a2d8978b65757e862af326846b550c2e314f821fb7485573b72ac18c2d20006c"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.50.1/Realm.spm.zip",
+            checksum: "942fc39917f4d572d5a2aae6a115c9f50a0954e61351aed3553c84d63ad3f2dc"
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.48.0/RealmSwift@15.3.spm.zip",
-            checksum: "36d860d326237b9950d2650f1bc9000b1af0540ce1a9967ae5d7f28be7956456"
-//            url: "https://github.com/realm/realm-swift/releases/download/v10.46.0/RealmSwift@15.2.spm.zip",
-//            checksum: "c429db7b2b6ea209a962930db7b28dea2134ccd7520daef9e79c6b8ca3d86a77"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.50.1/RealmSwift@15.4.spm.zip",
+            checksum: "208755d16d189372065e43901e5406e050197f98207142bc788487068f1a0843"
         )
     ]
 )
