6da0d09d154e6a16f4cee2bf8c2a34d71cd3e87b
Alex Ehlke <alex.ehlke@gmail.com>
2024-06-16 10:57:35 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "RealmBinary",
-    platforms: [.iOS(.v12), .macOS(.v10_14)],
+    platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         .library(
             name: "Realm",
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Realm",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.50.1/Realm.spm.zip",
-            checksum: "942fc39917f4d572d5a2aae6a115c9f50a0954e61351aed3553c84d63ad3f2dc"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.51.0/Realm.spm.zip",
+            checksum: "52c24b44863f83433592590035ba7cf55478cb076b659a68218e8d98648ef115"
         ),
         .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.50.1/RealmSwift@15.4.spm.zip",
-            checksum: "208755d16d189372065e43901e5406e050197f98207142bc788487068f1a0843"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.51.0/RealmSwift@15.4.spm.zip",
+            checksum: "b39f8b7ed1d432f3bc5c65649997d00676062a9e73e3fcda07d5146744c79736"
         )
     ]
 )
