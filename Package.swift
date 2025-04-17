ed38afe86eb16f6b1b8f027d90e227c1471112f8
Alex Ehlke <alex.ehlke@gmail.com>
2025-04-17 13:38:34 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
     targets: [
       .binaryTarget(
         name: "Realm",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.54.3/Realm.spm.zip",
-        checksum: "f2d3ddc1b4f19f8b81dbd1ed40e0b19aeab3c5925b2dab06c749adb5c003e022"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.54.4/Realm.spm.zip",
+        checksum: "41afcac96c88e860fe145e31cd15cc28f517afe7bc4c0f6b7de00303c1843c15"
       ),
       .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.54.3/RealmSwift@16.2.spm.zip",
-        checksum: "b6b9d80f6bc091257f95aa4b22362ea737c22b139d8f408039e60c251bf7f97d"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.54.4/RealmSwift@16.3.spm.zip",
+        checksum: "547ddf37e40ba16144604f000cbe6e835db936710aa4dc2db01feeb194b24c8f"
       ),
       .target(name: "_RealmStub"),
     ]
